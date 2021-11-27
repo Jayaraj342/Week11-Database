b0c41467a99d3bdbb2728485a8ddb1334ce3c35e
jayarajsg <jayarajsgoudar@gmail.com>
2021-11-27 20:07:10 +0530

    actuator added

--- admin-service/src/main/java/com/spring/boot/admin/service/config/SecurityConfiguration.java
+++ admin-service/src/main/java/com/spring/boot/admin/service/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.spring.boot.admin.service.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
